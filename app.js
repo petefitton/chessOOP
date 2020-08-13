2f1cdaf81ec0be84e690a50f205fb056075b0399
Pete Fitton <petefitton@gmail.com>
2020-08-12 20:58:20 -0700

    Make various changes
    Add notes for next steps at top of app.js
    
    Add id to Pawn constructor object and instantiations
    Add that piece id to the image tags associated with those objects
    Update classes for those to be blackPiece and whitePiece
    Move GameState constructor and gameState object to bottom of app.js
    
    Continue fleshing out GameState constructor function
    	-Add selectedPiece key
    	-firstPartTurn method
    	-secondPartTurn method
    		secondPartTurn set up test pure movement for now
    Instantiate gameState and call firstPartTurn method

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
+// NOTES FOR WHAT I WAS JUST WORKING ON:
 
+// need to create id's for each square
+// each chesspiece image tag also needs an id
+// these id's are separate things
+// places to update code are gameState object methods and instantiation of piece objects
+// plus Gameboard object for square id's
 
 
 
@@ -47,10 +53,6 @@ let gameboard = new Gameboard();
 
 
 
-
-
-
-
 // A pawn will need the following:
 // color, movement patterns, attack patterns, firstMove boolean for movement pattern purposes
 // movement handling, attack handling, checks for moving/attacking that prevent King from becoming in check
@@ -63,38 +65,32 @@ let gameboard = new Gameboard();
 
 
 
-// will need gameState Object 
-// handle turns
-// game board?
-// event listeners adding and removing based on whose turn and which part of turn (piece selection and then movement)
-// need to prevent movement when one's own King would be in check afterwards
-
-// special considerations:
-// en passant
-// castling
-// pawn first movement
-// black pawn vs white pawn direction
-
-
 function Piece() {
   this.alive = true;
 }
 
-function Pawn(color, location) {
+function Pawn(color, location, pieceId) {
   Piece.call(this);
   // movement changes boolean value
   this.hasMoved = false;
   this.color = color;
   this.location = location;
+  this.id = pieceId;
   this.moves = [/* info about one square forward or if firstMove can move two */];
   this.attack = [/* info about side-ways attack */];
   if (this.color === "black") {
     this.img_src = "./assets/Chess_pdt60.png";
+    this.image = document.createElement('img');
+    this.image.src = this.img_src;
+    this.image.classList.add('blackPiece')
+    this.image.id = pieceId
   } else {
     this.img_src = "./assets/Chess_plt60.png";
+    this.image = document.createElement('img');
+    this.image.src = this.img_src;
+    this.image.classList.add('whitePiece')
+    this.image.id = pieceId
   }
-  this.image = document.createElement('img');
-  this.image.src = this.img_src;
   this.render = function(imgNode, location) {
     document.getElementById(location).appendChild(imgNode);
   }(this.image, this.location)
@@ -103,23 +99,23 @@ function Pawn(color, location) {
 Pawn.prototype = Object.create(Piece.prototype);
 Pawn.prototype.constructor = Pawn;
 
-let blackPawnOne = new Pawn("black", 8);
-let blackPawnTwo = new Pawn("black", 9);
-let blackPawnThree = new Pawn("black", 10);
-let blackPawnFour = new Pawn("black", 11);
-let blackPawnFive = new Pawn("black", 12);
-let blackPawnSix = new Pawn("black", 13);
-let blackPawnSeven = new Pawn("black", 14);
-let blackPawnEight = new Pawn("black", 15);
+let blackPawnOne = new Pawn("black", 8, 1);
+let blackPawnTwo = new Pawn("black", 9, 2);
+let blackPawnThree = new Pawn("black", 10, 3);
+let blackPawnFour = new Pawn("black", 11, 4);
+let blackPawnFive = new Pawn("black", 12, 5);
+let blackPawnSix = new Pawn("black", 13, 6);
+let blackPawnSeven = new Pawn("black", 14, 7);
+let blackPawnEight = new Pawn("black", 15, 8);
 
-let whitePawnOne = new Pawn("white", 48);
-let whitePawnTwo = new Pawn("white", 49);
-let whitePawnThree = new Pawn("white", 50);
-let whitePawnFour = new Pawn("white", 51);
-let whitePawnFive = new Pawn("white", 52);
-let whitePawnSix = new Pawn("white", 53);
-let whitePawnSeven = new Pawn("white", 54);
-let whitePawnEight = new Pawn("white", 55);
+let whitePawnOne = new Pawn("white", 48, 17);
+let whitePawnTwo = new Pawn("white", 49, 18);
+let whitePawnThree = new Pawn("white", 50, 19);
+let whitePawnFour = new Pawn("white", 51, 20);
+let whitePawnFive = new Pawn("white", 52, 21);
+let whitePawnSix = new Pawn("white", 53, 22);
+let whitePawnSeven = new Pawn("white", 54, 23);
+let whitePawnEight = new Pawn("white", 55, 24);
 
 
 
@@ -277,3 +273,89 @@ let whiteQueenOne = new Queen("white", 59);
 
 
 
+
+
+
+
+
+
+// will need gameState Object 
+// handle turns
+// game board?
+// event listeners adding and removing based on whose turn and which part of turn (piece selection and then movement)
+// need to prevent movement when one's own King would be in check afterwards
+
+// special considerations:
+// en passant
+// castling
+// pawn first movement
+// black pawn vs white pawn direction
+
+
+function GameState() {
+  this.isWhiteTurn = true;
+  this.selectedPiece = null;
+
+  this.firstPartTurn = function() {
+    if (gameState.isWhiteTurn) { 
+      let playablePieces = document.querySelectorAll('.whitePiece');
+      playablePieces.forEach(piece => {
+        piece.addEventListener('click', callbackerooni);
+      })
+    } else {
+      let playablePieces = document.querySelectorAll('.blackPiece');
+      playablePieces.forEach(piece => {
+        piece.addEventListener('click', callbackerooni);
+      })
+    }
+    function callbackerooni(e) {
+      let playablePieces
+      if (gameState.isWhiteTurn) { 
+        playablePieces = document.querySelectorAll('.whitePiece');
+      } else {
+        playablePieces = document.querySelectorAll('.blackPiece');
+      }
+      console.log(playablePieces)
+      playablePieces.forEach(piece => {
+        piece.removeEventListener('click', callbackerooni);
+      })
+      gameState.selectedPiece = e.target;
+      console.log(gameState.selectedPiece)
+      return gameState.secondPartTurn()
+    }
+  }
+
+  this.secondPartTurn = function() {
+    console.log(this.selectedPiece)
+    // handle movement
+    // add event listener to every square that selectedPiece can move to
+    // each piece is an object that has information regarding where it can move/capture
+
+
+
+    // first test simple scenario
+    let squares = document.querySelectorAll('.square');
+    squares.forEach(square => {
+      square.addEventListener('click', callerbacker);
+    })
+    function callerbacker(e) {
+      let squares = document.querySelectorAll('.square');
+      squares.forEach(square => {
+        square.removeEventListener('click', callerbacker);
+      })
+      // let selectedSquare = e.target;
+      // gameState.selectedPiece is the image tag associated with the selectedPiece object
+      // need to grab object from image
+      // give each image tag a unique chess piece id and then I will be able to easily go from one to the other
+      gameState.selectedPiece.id = e.target.id;
+      gameState.selectedPiece.render();
+      return gameState.firstPartTurn()
+    }
+  }
+}
+
+let gameState = new GameState();
+
+
+
+gameState.firstPartTurn();
